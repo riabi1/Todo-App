90acca71af42627bacbb10cf9c1a66d6666237d6
riabi1 <riabi.mar@gmail.com>
2024-07-03 12:51:54 +0100

    second commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
+import "./index.css";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+  <div>
+    <TodoList />
+  </div>,
+  destination
+);
